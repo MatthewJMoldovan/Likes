0d0b7fc8930c4af031638429f901fa1014d64b60
Matthew J. Moldovan <matthewmoldovan@outlook.com>
2023-01-31 13:48:54 -0700

    Likes

---
+++ script.js
@@ -0,0 +1,17 @@
+var likeNeil = 9;
+function addLikeNeil(){
+    likeNeil +=1;
+    document.getElementById("neilLikes").innerHTML = likeNeil;
+}
+
+var likeNichole = 12;
+function addLikeNichole(){
+    likeNichole +=1;
+    document.getElementById("nicholeLikes").innerHTML = likeNichole;
+}
+
+var likeJim = 9;
+function addLikeJim(){
+    likeJim +=1;
+    document.getElementById("jimLikes").innerHTML = likeJim;
+}
